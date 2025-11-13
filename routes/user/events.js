58d6e2c9d84bffbecd323b47f308e71667295f26
avinayk <avinaykumar.web@gmail.com>
2025-11-13 15:36:39 +0530

    frontend

--- routes/user/events.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const EventsController = require("../../controllers/user/EventsController");
-
-// Define the POST /login route
-router.post("/createevent", EventsController.createevent);
-
-module.exports = router;
